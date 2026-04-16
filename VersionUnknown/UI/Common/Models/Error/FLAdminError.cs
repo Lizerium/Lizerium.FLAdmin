411d8ec1d47e11095b8b73e5ed443d4b02518760
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-16 11:44:04 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-16

--- VersionUnknown/UI/Common/Models/Error/FLAdminError.cs
+++ VersionUnknown/UI/Common/Models/Error/FLAdminError.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and TheStarport
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 апреля 2026 06:52:12
- * Version: 1.0.173
+ * Last Updated: 16 апреля 2026 11:43:48
+ * Version: 1.0.174
  */
 
 using System.ComponentModel;
