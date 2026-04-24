b2c5c0fcb5257fc6575c789a03d8f4fbc1081fb4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-24 06:52:25 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-24

--- VersionStaport/Logic/Services/Database/DatabaseService.cs
+++ VersionStaport/Logic/Services/Database/DatabaseService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky and TheStarport
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 апреля 2026 06:52:58
- * Version: 1.0.182
+ * Last Updated: 24 апреля 2026 06:52:11
+ * Version: 1.0.183
  */
 
 namespace FlAdmin.Logic.Services.Database;
